608e13a56ab539649ca3dbd046fcb80e3e77ffe0
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-16 15:39:17 -0700

    Game is working

--- js/dice-game.js
+++ js/dice-game.js
@@ -3,5 +3,19 @@ function rollDice() {
   for (i = 0; i < 10; i++) {
     const roll = Math.floor(Math.random() * 6) + 1;
     alert("You rolled a " + roll + "!");
+    if (roll === 1) {
+      alert("Game over, no more rolls!");
+      break;
+    }
+    if (roll < 5) {
+      continue;
+    }
+    if (roll === 5) {
+      alert("You win 5 gold coins!");
+    } else if (roll === 6) {
+      alert("You win 6 gold coins!");
+    }
+    goldCoins += roll;
   }
+  alert("You won a total of " + goldCoins + " coins!");
 }
